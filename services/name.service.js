8f477608a8bcee5475b1c477c6739fec2d71405c
James Marlow <jim@grinwithoutacat.com>
2021-11-15 22:59:10 -0800

    Working example of selecting names at random and returning them!

--- services/name.service.js
+++ services/name.service.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const fs = require('fs')
+
+function between(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
 
 module.exports = { 
     getName: (req, res) => {
+        fileName = __dirname + '/data/names.txt'
+        data = fs.readFileSync(fileName, 'utf8').toString().split('\n')
+        
+        fnPos = between(0, data.length - 1)
+        lnPos = between(0, data.length - 1)
+
+        firstName = data[fnPos]
+        lastName = data[lnPos]
         const name = {
-            "First": "First Name",
-            "Last": "Last Name",
-            "Full": "First Name Last Name"
+            "First": firstName,
+            "Last": lastName,
+            "Full": firstName + ' ' + lastName
         }
 
         return res.status(200).json({name: name})
