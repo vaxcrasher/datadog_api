7c3861dccea042ba49789f30d265a240e07be797
James Marlow <jim@grinwithoutacat.com>
2021-11-18 11:53:17 -0800

    Modularized code in preparation for adding other functions

--- services/words.service.js
+++ services/words.service.js
@@ -10,11 +10,7 @@ const errorEntropy = 100
 const delayEntropy = 100
 const delayMax = 30000
 
-module.exports = { 
-    // getWord - reads a list of names and picks a first and last name from the list. Returns the data in JSON
-    getWord: (req, res) => {
-        log.info('Getting word...') 
-
+function getSingleWord() {
         // read the file
         fileName = __dirname + '/data/words.txt'
         data = fs.readFileSync(fileName, 'utf8').toString().split('\n')
@@ -24,40 +20,65 @@ module.exports = {
         word = data[wordPos]
         log.info('Word pos: ' + wordPos)
 
-        // build the return string
-        const name = {
-            "Word": word
-        }
+        // return the word
+        return word
+}
+
+function getSingleSentence() {
+
+}
+
+function getSingleParagraph() {
+
+}
+
+function delayScript() {
+    // add a random delay (times in ms)
+    delayStatus = lib.between(1, delayEntropy)
+    if (delayStatus == 1) {
+        delayTime = lib.between(1000, delayMax)
+        log.warn('Delaying for ' + delayTime + ' ms')
+        lib.sleep(delayTime)
+    }
+}
+
+function sendResponse(res, valueType, value) {
+    // randomly cause an error based on errorEntropy (400, 403, 500, 503) or send a normal response
+    errorStatus = lib.between(1, errorEntropy)
+    if (errorStatus < 5) { 
+        log.warn(valueType + ': errorStatus is less than 5 at ' + errorStatus)
+    }
+    switch (errorStatus) {
+        case 1:
+            log.error(valueType + ': 400 Bad Request triggered by random error ' + errorStatus)
+            return res.status(400).send("400 Bad Request")
+        case 2:
+            log.error(valueType + ': 403 Forbidden triggered by random error ' + errorStatus)
+            return res.status(403).send("403 Forbidden")
+        case 3:
+            log.error(valueType + ': 500 Server Error triggered by random error ' + errorStatus)
+            return res.status(500).send("500 Server Error")
+        case 4:
+            log.error(valueType + ': 503 Server Temporarily Unavailable triggered by random error ' + errorStatus)
+            return res.status(503).send("503 Server temporarily unavailable")
+        default:
+            return res.status(200).json({value: value})
+    }
+}
+
+module.exports = { 
+    // getWord - reads a list of names and picks a first and last name from the list. Returns the data in JSON
+    getWord: (req, res) => {
+        log.info('Getting word...') 
+        word = getSingleWord()
         log.info(word)
 
-        // add a random delay (times in ms)
-        delayStatus = lib.between(1, delayEntropy)
-        if (delayStatus == 1) {
-            delayTime = lib.between(1000, delayMax)
-            log.warn('Delaying for ' + delayTime + ' ms')
-            lib.sleep(delayTime)
-        }
+        // delay script
+        delayScript()
 
-        // randomly cause an error based on errorEntropy (400, 403, 500, 503) or send a normal response
-        errorStatus = lib.between(1, errorEntropy)
-        if (errorStatus < 5) { 
-            log.warn('errorStatus is less than 5 at ' + errorStatus)
-        }
-        switch (errorStatus) {
-            case 1:
-                log.error('400 Bad Request triggered by random error ' + errorStatus)
-                return res.status(400).send("400 Bad Request")
-            case 2:
-                log.error('403 Forbidden triggered by random error ' + errorStatus)
-                return res.status(403).send("403 Forbidden")
-            case 3:
-                log.error('500 Server Error triggered by random error ' + errorStatus)
-                return res.status(500).send("500 Server Error")
-            case 4:
-                log.error('503 Server Temporarily Unavailable triggered by random error ' + errorStatus)
-                return res.status(503).send("503 Server temporarily unavailable")
-            default:
-                return res.status(200).json({word: word})
-        }
+        // send the response
+        sendResponse(res, "word", word)
     }
+
+
 }
